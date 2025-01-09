1ab31752be1c770f7d0366869173f668333b59d0
Rabbi-rc <suleimancharles67@gmail.com>
2025-01-09 03:33:22 +0300

    Update Reboot.js

--- Andbad_cmds/Reboot.js
+++ Andbad_cmds/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("KYPHER_XMD bot Restarting ⏳");
+    repondre("RABBI_XMD bot Restarting ⏳");
 
   exec("pm2 restart all");
   
