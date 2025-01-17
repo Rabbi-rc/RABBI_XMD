42ec9357db93432f3ce15f40cf792d6a813de260
Rabbi-rc <suleimancharles67@gmail.com>
2025-01-17 18:04:12 +0300

    Update ping.js

--- Andbad_cmds/ping.js
+++ Andbad_cmds/ping.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "ping", reaction: "🫀", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'KYPHER_XMD IS ALIVE... \n\n\nGO GITHUB AND SEARCH KYPHER_XMD FORK THE REPO DEPLOY AND DM FOR YOUR REWARD\n\n\n';
-    let d = '                                                                           CREATED BY KYPHER HIMSELF';
+    let z = 'RABBI_XMD IS ALIVE... \n\n\nGO GITHUB AND SEARCH RABBI_XMD FORK THE REPO DEPLOY AND DM FOR YOUR REWARD\n\n\n';
+    let d = '                                                                           CREATED BY RABBI HIMSELF';
     let varmess = z + d;
     var video = 'https://i.imgur.com/UfpoNZT.mp4';
     await zk.sendMessage(dest, { video: { url: img }, caption: varmess });
